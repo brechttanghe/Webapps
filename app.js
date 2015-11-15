b6ff42597432d71e72090f2ff9532e5802fa2cc1
brecht tanghe <brecht_tanghe@hotmail.com>
2015-11-15 17:40:17 +0100

    Post Page

--- app.js
+++ app.js
@@ -5,30 +5,52 @@ app.controller('MainCtrl', [
 	'posts',
 	function($scope , posts){
 		$scope.posts = posts.posts
-
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){return;}
 			$scope.posts.push({
 				title: $scope.title, 
 				link: $scope.link,
-				upvotes: 0});
+				upvotes: 0 ,
+				comments: [
+    				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  				]
+			});
 			$scope.title ='';
 			$scope.link = '';
-
 		};
 
 		$scope.incrementUpvotes = function(post){
 			post.upvotes += 1;
 		};
 
+
 		
 }]);
 
+app.controller('PostsCtrl' ,[
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+	$scope.addComment = function(){
+		if($scope.body === ''){return;}
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
+	};
+}]);
+
+
 app.factory('posts', [function(){
-			var postFactory = {
-				posts:[]
-		};
-		return postFactory; 
+	var postFactory = {
+		posts:[]
+	};
+	return postFactory; 
 }]);
 
 app.config([
@@ -41,7 +63,13 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+    .state('posts', {
+    	url:'/posts/{id}',
+    	templateUrl: '/posts.html',
+  		controller: 'PostsCtrl'
     });
 
   $urlRouterProvider.otherwise('home');
 }]);
+

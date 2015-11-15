ec81b65c96e38b56763148244168535191ca0c59
brecht tanghe <brecht_tanghe@hotmail.com>
2015-11-15 15:45:45 +0100

    Angular Routing

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 app.controller('MainCtrl', [
 	'$scope',
-	function($scope){
-		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
-			{title: 'post 2', upvotes: 4},
-			{title: 'post 3', upvotes: 9},
-			{title: 'post 4', upvotes: 2},
-			{title: 'post 5', upvotes: 7},
-		];
+	'posts',
+	function($scope , posts){
+		$scope.posts = posts.posts
 
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === ''){return;}
@@ -26,10 +21,27 @@ app.controller('MainCtrl', [
 			post.upvotes += 1;
 		};
 
-		app.factory('posts', [function(){
+		
+}]);
+
+app.factory('posts', [function(){
 			var postFactory = {
 				posts:[]
-			};
-			return postFactory; 
-		}])
-	}]);
+		};
+		return postFactory; 
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);

8ba4d4741fc656b952aebce0c32ce20189e5001e
brecht tanghe <brecht_tanghe@hotmail.com>
2015-12-04 13:05:03 +0100

    refactoring controllers

---
+++ public/javascripts/controllers/authCtrl.js
@@ -0,0 +1,27 @@
+app.controller('AuthCtrl', ['$scope', '$state', 'auth',
+function($scope, $state, auth) {
+	$scope.user = {};
+
+	$scope.register = function() {
+		auth.register($scope.user).error(function(error) {
+			$scope.error = error;
+		}).then(function() {
+			$state.go('home');
+		});
+	};
+
+	$scope.logIn = function() {
+		auth.logIn($scope.user).error(function(error) {
+			$scope.error = error;
+		}).then(function() {
+			$state.go('home');
+		});
+	};
+}]);
+
+app.controller('NavCtrl', ['$scope', 'auth',
+function($scope, auth) {
+	$scope.isLoggedIn = auth.isLoggedIn;
+	$scope.currentUser = auth.currentUser;
+	$scope.logOut = auth.logOut;
+}]);
